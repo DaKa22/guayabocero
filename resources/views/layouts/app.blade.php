bc2d65d7498c760d64a734170222752270e570f2
David Bermeo <davidbermeoquimbaya22dbqq@gmail.com>
2022-11-28 10:52:24 -0500

    se realizaron arreglos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Laralink">
+  <meta name="author" content="Daka22">
   {{-- <link rel="icon" href="{{ asset('assets/img/favicon.png') }}" /> --}}
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
